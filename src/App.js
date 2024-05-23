20aff08cfb0eae512f209313a4287d0cf961353a
sanjukumarkanki <sanjukanki56429@gmail.com>
2024-05-23 06:42:33 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import Cookies from "js-cookie";
 import DaywiseFollowups from "./components/DaywiseFollowups";
 import ReactContext from "./contexts";
 
-// export const baseUrl = "https://cms-backend-difl.onrender.com";
+export const baseUrl = "https://cms-backend-difl.onrender.com";
 
-export const baseUrl = "http://localhost:3003";
+// export const baseUrl = "http://localhost:3003";
 
 // To get the stored token from browser
 export const token = Cookies.get("token");
